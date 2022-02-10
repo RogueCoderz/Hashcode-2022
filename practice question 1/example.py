6d519d58050598a4b7ae91d3f93c877c55a3310e
Aabhas Kumar Jha <abhasjha03@gmail.com>
2022-02-10 20:43:52 +0530

    Practice Question 1

---
+++ practice question 1/example.py
@@ -0,0 +1,36 @@
+with open("input_data/c_coarse.in.txt", "r") as f:
+    input_str = f.read().split()[1:]
+
+like_set = set()
+dislike_set = set()
+
+like = True
+index = 0
+indexing = True
+
+while indexing:
+
+    if like:
+        n = int(input_str[index])
+        if n != 0:
+            for i in range(index+1, index+n+1):
+                like_set = like_set.union({input_str[i]})
+        like = False
+        index = index + n + 1
+
+    else:
+        n = int(input_str[index])
+        if n != 0:
+            for i in range(index+1, index+n+1):
+                dislike_set = dislike_set.union({input_str[i]})
+        like = True
+        index = index + n + 1
+
+    if index == len(input_str):
+        indexing = False
+
+common = like_set.intersection(dislike_set)
+ingredients = like_set.difference(common)
+
+out_str = " ".join(ingredients)
+print(f"{len(ingredients)} {out_str}")
---
+++ practice question 1/main.py
@@ -0,0 +1,16 @@
+n =  int(input())
+
+like_set = set()
+dislike_set = set()
+
+for _ in range(n):
+    likes = set(input().split(" ")[1:])
+    dislikes = set(input().split(" ")[1:])
+    like_set = like_set.union(likes)
+    dislike_set = dislike_set.union(dislikes)
+
+common = like_set.intersection(dislike_set)
+ingredients = like_set.difference(common)
+
+out_str = " ".join(ingredients)
+print(f"{len(ingredients)} {out_str}")
