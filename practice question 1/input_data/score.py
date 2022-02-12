ed665ac2fa61df0c3a530066267d5e866d5f7320
Aabhas Kumar Jha <abhasjha03@gmail.com>
2022-02-12 23:06:52 +0530

    added score calculator for output

---
+++ practice question 1/input_data/score.py
@@ -0,0 +1,43 @@
+import linecache
+
+input_filename = "a_an_example.in.txt"
+output_filename="Ans.txt" 
+
+
+pl = int(linecache.getline(input_filename, 1))
+with open(input_filename,'r') as f:  
+  list_indi = [[0 for a in range(2)] for b in range(pl)] 
+  def find_ingerdiants(list_indi):
+    a=0
+    for i in f:      
+      a+=1
+      if len(i)>2:
+        k = i.split()
+        if (a%2==0):
+          k.pop(0)          
+          list_indi[(a//2)-1][0]=k          
+        else:
+          k.pop(0)  
+          list_indi[(a//2)-1][1]=k          
+           
+  find_ingerdiants(list_indi)
+
+input_file = open(output_filename,"r")
+lst = input_file.read().split()
+lst.pop(0)
+
+customer=0
+for i in range(0,pl):
+  a=0
+  
+  if list_indi[i][1] != 0:
+
+    for j in range(0,len(list_indi[i][1])):
+      if list_indi[i][1][j] in lst:
+        a+=1
+    if a>0:
+      continue  
+  if (set(list_indi[i][0]).issubset(set(lst))):
+    customer+=1
+    
+print(customer)
